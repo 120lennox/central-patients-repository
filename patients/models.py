b068c649bf716babab3298f5c79a26f97d0211d6
Lennox Mountain <lennoxmountain67@gmail.com>
2026-03-21 21:29:11 +0200

    patient model with fhir standard

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.db.models import Max
+import uuid
+import random
 
 # Create your models here.
+User = get_user_model()
+
+class Patient(models.Model):
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+        ('unknown', 'Unknown'),
+    ]
+
+    # identifiers 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    patient_id = models.CharField(max_length=30, unique=True, editable=False, db_index=True)
+    national_id = models.CharField(max_length=30, unique=True, null=True, blank=True, db_index=True)
+    digital_id = models.CharField(max_length=30, unique=True, null=True, blank=True, db_index=True)
+
+    # user account link
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='patient_profile')
+    account_claimed = models.BooleanField(default=False)
+
+    # patient names fhir standard
+    
+
